49c978086c2f4a5ef022cc3af8959cc513ea6971
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2019-05-09 12:16:21 +0200

    fix: bump postgres timeouts to 3 minutes to test first
    Tentatively bumping at first to see if this helps out

--- apps/omg_watcher/config/dev.exs
+++ apps/omg_watcher/config/dev.exs
@@ -43,8 +43,8 @@ config :phoenix, :stacktrace_depth, 20
 config :omg_watcher, OMG.Watcher.DB.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool_size: 10,
-  timeout: 60_000,
-  connect_timeout: 60_000,
+  timeout: 180_000,
+  connect_timeout: 180_000,
   # DATABASE_URL format is following `postgres://{user_name}:{password}@{host:port}/{database_name}`
   url: {:system, "DATABASE_URL", "postgres://omisego_dev:omisego_dev@localhost/omisego_dev"},
   loggers: [Appsignal.Ecto, Ecto.LogEntry]
