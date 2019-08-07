8f62b55e6e90b4d46659bf9f78782fd4b14ccdcd
Paul Peregud <paulperegud@gmail.com>
2019-08-07 12:13:17 +0200

    drop python, update erlang and elixir

--- default.nix
+++ default.nix
@@ -5,16 +5,10 @@ with pkgs;
 let
   # I like to define variables for derivations that have
   # a specific version and are subject to change over time.
-  elixir = beam.packages.erlangR21.elixir_1_7;
+  elixir = beam.packages.erlangR22.elixir_1_9;
   libsecp256k1 = secp256k1.secp256k1-2017-12-18;
-  contractbuilding = ps: [
-    ps.requests
-    ps.py-solc
-    ps.py-solc-simple
-  ];
-  pythonForContracts = python3.withPackages contractbuilding;
 in
 
 mkShell {
-  propagatedBuildInputs = [ elixir git erlangR21 gmp secp256k1 solc pythonForContracts ];
+  propagatedBuildInputs = [ elixir git cmake erlangR22 gmp secp256k1 solc ];
 }
